2676cac001cf9e87b91c424f6476cdea2641dd42
alikameli <alikameli0123@gmail.com>
2023-10-28 09:36:57 +0330

    fanout exchange type

--- consumer.js
+++ consumer.js
@@ -1,12 +1,20 @@
 const amqp = require("amqplib");
 
-async function recivedFromProducer() {
-  const queueName = "service1";
+const exchangeName = "logs";
+
+async function recivedMsg() {
   const connection = await amqp.connect("amqp://localhost:5672");
   const channel = await connection.createChannel();
-  await channel.consume(queueName, (msg) => {
-    console.log(msg.content.toString());
-  });
+  await channel.assertExchange(exchangeName, "fanout");
+  const assertedQueue = await channel.assertQueue("", { exclusive: true });
+  console.log('binding queue with queueName: ',assertedQueue.queue);
+  channel.bindQueue(assertedQueue.queue, exchangeName,"");
+  channel.consume(assertedQueue.queue, msg=>{
+    if(msg.content){
+      console.log(msg.content.toString());
+      channel.ack(msg)
+    }
+  })
 }
 
-recivedFromProducer();
+recivedMsg();
--- producer.js
+++ producer.js
@@ -1,11 +1,16 @@
 const amqp = require("amqplib");
 
-async function connectToService1() {
-  const queueName = "service1";
+const exchangeName = "logs";
+
+async function sendMsg() {
   const connection = await amqp.connect("amqp://localhost:5672");
   const channel = await connection.createChannel();
-  await channel.assertQueue(queueName, { durable: true });
-  channel.sendToQueue(queueName, Buffer.from("Rabbit Test"));
+  await channel.assertExchange(exchangeName, "fanout");
+  await channel.publish(exchangeName, "", Buffer.from("data from fanout"));
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 1000);
 }
 
-connectToService1();
+sendMsg();
